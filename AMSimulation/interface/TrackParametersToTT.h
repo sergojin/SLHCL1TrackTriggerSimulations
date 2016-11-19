e61eafc8efc1a951f362c217c12628a01d48fed4
Jia Fu Low <jia.fu.low@cern.ch>
2016-11-19 12:53:05 -0600

    Make TrackParametersToTT header-only for easy use

--- AMSimulation/interface/TrackParametersToTT.h
+++ AMSimulation/interface/TrackParametersToTT.h
@@ -1,6 +1,9 @@
 #ifndef AMSimulation_TrackParametersToTT_h_
 #define AMSimulation_TrackParametersToTT_h_
 
+#include <cmath>
+#include <cassert>
+
 // Codes originally written by Olmo Cerri (SNS Pisa)
 // See https://github.com/ocerri/SLHCL1TrackTriggerSimulations/blob/dev_tt25/AMSimulation/src/TrackParametersToTT.h
 // Modified for better coherence with the rest of AMSimulation
@@ -9,13 +12,64 @@ namespace slhcl1tt {
 
   class TrackParametersToTT {
   public:
-    int get_tt(double phi, double invPt, double eta, double vz);
+    int get_tt(double phi, double invPt, double eta, double z0) {
+      constexpr double max_eta = 2.2;
+      constexpr double max_z0 = 15.; // [cm]
+      constexpr double max_invPt = 1./3;  // [1/GeV]
+      double etaStar = get_etaStar_from_eta(eta, z0, invPt);
+      double phiStar = get_phiStar_from_phi(phi, invPt);
+      if (std::abs(etaStar) > max_eta || std::abs(z0) > max_z0 || std::abs(invPt) > max_invPt) {
+        return -1;
+      }
 
-    double get_phiStar_from_phi(double phi, double invPt, double rStar=90.);
-    double get_phi_from_phiStar(double phiStar, double invPt, double rStar=90.);
+      int tt_eta = (etaStar + max_eta) / (max_eta*2./6);
+      int tt_phi = (phiStar + M_PI) / (M_PI*2./8);
+      int tt = tt_eta * 8 + tt_phi;
+      assert(0<=tt && tt<48);
+      return tt;
+    }
 
-    double get_etaStar_from_eta(double eta, double vz, double invPt, double rStar=60.);
-    double get_eta_from_etaStar(double etaStar, double vz, double invPt, double rStar=60.);
+    double get_phiStar_from_phi(double phi, double invPt, double rStar=90.) {
+      constexpr double mPtFactor = 0.3*3.8*1e-2/2.0;
+      double dphi = - asin(mPtFactor * rStar * invPt);
+      double phiStar = phi + dphi;
+      while (phiStar < -M_PI)
+        phiStar += M_PI*2.;
+      while (phiStar >= M_PI)
+        phiStar -= M_PI*2.;
+      return phiStar;
+    }
+
+    double get_phi_from_phiStar(double phiStar, double invPt, double rStar=90.) {
+      constexpr double mPtFactor = 0.3*3.8*1e-2/2.0;
+      double dphi = - asin(mPtFactor * rStar * invPt);
+      double phi = phiStar - dphi;
+      while (phi < -M_PI)
+        phi += M_PI*2.;
+      while (phi >= M_PI)
+        phi -= M_PI*2.;
+      return phi;
+    }
+
+    double get_etaStar_from_eta(double eta, double z0, double invPt, double rStar=60.) {
+      constexpr double mPtFactor = 0.3*3.8*1e-2/2.0;
+      if (std::abs(rStar) < 1e-10)  return eta;
+      if (std::abs(invPt) < 1e-10 && invPt <  0.) invPt = -1e-10;
+      if (std::abs(invPt) < 1e-10 && invPt >= 0.) invPt = +1e-10;
+      double cot = sinh(eta);
+      double cotStar = (cot * (asin(mPtFactor * rStar * invPt)/(mPtFactor * invPt)) + z0) / rStar;
+      return asinh(cotStar);
+    }
+
+    double get_eta_from_etaStar(double etaStar, double z0, double invPt, double rStar=60.) {
+      constexpr double mPtFactor = 0.3*3.8*1e-2/2.0;
+      if (std::abs(rStar) < 1e-10)  return etaStar;
+      if (std::abs(invPt) < 1e-10 && invPt <  0.) invPt = -1e-10;
+      if (std::abs(invPt) < 1e-10 && invPt >= 0.) invPt = +1e-10;
+      double cotStar = sinh(etaStar);
+      double cot = (rStar * cotStar - z0) / (asin(mPtFactor * rStar * invPt)/(mPtFactor * invPt));
+      return asinh(cot);
+    }
   };
 
 }  // namespace slhcl1tt
